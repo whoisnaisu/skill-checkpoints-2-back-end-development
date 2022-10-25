5683b62022c869c62edd7cced00370658fc73a50
naisu <napas14204@gmaill.com>
2022-10-25 14:41:06 +0700

    fix: SQL

--- sql/created_table_users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users(
-user_id INT PRIMARY KEY generated always as identity,
-username varchar ( 25 ) unique NOT NULL,
-password varchar ( 25 ) NOT NULL,
-firstname varchar ( 100 ) NOT NULL,
-lastname varchar ( 100 ) NOT NULL,
-email varchar ( 100 ) NOT NULL,
-created_at TIMESTAMPTZ NOT NULL
-);
